e13157f84d65b5c0e4c91417a5dd3447f3faec2a
Shikhar <f20170794@hyderabad.bits-pilani.ac.in>
2020-11-18 18:43:07 +0530

    Refreshing everything.

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.get(
   '/deeplink',
   deeplink({
     fallback: 'https://explore.mapmyindia.com/',
+    android_package_name: 'com.prayerrntesting',
+    ios_store_link:
+      'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4',
   })
 );
 
